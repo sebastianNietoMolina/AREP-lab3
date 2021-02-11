7c6700c44ce7045d84f062dce33d39877af6b921
sebastianNietoMolina <juan.nieto-mo@mail.escuelaing.edu.co>
2021-02-11 02:53:31 -0500

    reto1

--- src/main/java/edu/escuelaing/arep/app/edu/escuelaing/arep/httpserver/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/app/edu/escuelaing/arep/httpserver/HttpServer.java
@@ -5,9 +5,15 @@ import java.io.*;
 
 public class HttpServer {
 
-    public static boolean isAlive = true;
+    private static boolean isAlive = true;
 
+    /**
+     * Servidor de peticiones http.
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
+
         ServerSocket serverSocket = null;
         try {
             serverSocket = new ServerSocket(getPort());
@@ -26,18 +32,33 @@ public class HttpServer {
                 System.exit(1);
             }
 
-            PrintWriter out = new PrintWriter(
-                    clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(clientSocket.getInputStream()));
-            String inputLine, outputLine;
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine;
+            String outputLine;
+
             while ((inputLine = in.readLine()) != null) {
                 System.out.println("Recibí: " + inputLine);
                 if (!in.ready()) {
                     break;
                 }
+                if (inputLine.contains("GET")){
+                    String[] typeData = inputLine.split(" ");
+                    String data = typeData[1];
+                    if(data.equals("/hello.html")){
+                        doHello(out);
+                    }else if(data.equals("/dogs.jpg") || data.equals("/dogs.JPG")){
+                        doDogs(out);
+                    }else if(data.contains("/cats.png") || data.contains("/cats.PNG")){
+                        doCats(out);
+                    }else{
+                        notFound(out);
+                    }
+
+                }
             }
 
+            /*
             outputLine = "HTTP/1.1 200 OK\r\n"
                     + "Content-Type: text/html\r\n"
                     + "\r\n"
@@ -53,24 +74,131 @@ public class HttpServer {
                     + "</html>\n";
             out.println(outputLine);
             out.close();
+             */
             in.close();
             clientSocket.close();
         }
         serverSocket.close();
     }
 
+    /**
+     * Nos dice si el servidor esta prendido o apagado.
+     * @return boolean.
+     */
     private static boolean isAlive() {
         return isAlive;
     }
 
+    /**
+     *  Nos permite cambiar el estado del servidor, de apagado a prendido, o al contrario.
+     * @param alive
+     */
     private static void setAlive(boolean alive) {
         isAlive = alive;
+
     }
 
+    /**
+     * html estático que está contenido en disco.
+     * @param out
+     */
+    public static void doHello(PrintWriter out){
+        String outputLine;
+        outputLine = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title>Hello!!!</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<h1>Very good days!</h1>\n"
+                + "</body>\n"
+                + "</html>\n";
+        out.println(outputLine);
+        out.close();
+    }
+
+    /**
+     * html estático que nos devuelve una imagen de perros
+     * @param out
+     */
+    public static void doDogs(PrintWriter out){
+        String outputLine;
+        outputLine = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title>Dogs!!</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<img src=\"https://www.happets.com/blog/wp-content/uploads/2019/08/ventajas-de-un-dispensador-de-comida-para-perros.jpg\" />\n"
+                + "</body>\n"
+                + "</html>\n";
+        out.println(outputLine);
+        out.close();
+    }
+
+    /**
+     * html estático que nos devuelve una imagen de gatos.
+     * @param out
+     */
+    public static void doCats(PrintWriter out){
+        String outputLine;
+        outputLine = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title>Title of the document</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<img src=\"https://www.pngkey.com/png/detail/909-9090660_adopt-cat-imagens-de-gatos-png.png\" />\n"
+                + "</body>\n"
+                + "</html>\n";
+        out.println(outputLine);
+        out.close();
+    }
+
+    /**
+     * Método que nos retorna error en caso de no encontrar una petición en disco.
+     * @param out
+     */
+    public static void notFound(PrintWriter out) {
+        String outputLine;
+        outputLine = "HTTP/1.1 404 not Found\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title>Error</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<h1>Does not exist</h1>\n"
+                + "</body>\n"
+                + "</html>\n";
+        out.println(outputLine);
+        out.close();
+    }
+
+    /**
+     * Método que nos devuelve el puerto por el que correrá localmente nuestro servicio
+     * @return el puerto del servicio.
+     */
     private static int getPort(){
         if(System.getenv("PORT") != null){
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 36000;
     }
+
 }
